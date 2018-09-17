d5932e0e5afa488389f28fa80324f824e4bc09cc
Xikun <emmettwb@gmail.com>
2018-09-16 18:06:47 -0600

    even moar logging

--- server.js
+++ server.js
@@ -123,6 +123,8 @@ io.on('connection', function(socket){
 
   socket.on('get rooms', function(data){
     rep = [];
+    console.log(data);
+    console.log(authList[data]);
     for (room in authList[data]['rooms']) {
       rep.push([room, rooms[room]]);
     }
