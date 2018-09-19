3834ca626202024b273c50421333c3617752c583
[R3dact3d] <emmettwb@gmail.com>
2018-09-19 13:48:27 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -7,29 +7,29 @@ var alreadyLoggedIn = false
 
 /// Functions
 function hashCode(s) {
-    for(var i=0, h=1; i<s.length; i++)
-        h=Math.imul(h+s.charCodeAt(i)|0, 2654435761);
-    return (h^h>>>17)>>>0;
+  for(var i=0, h=1; i<s.length; i++)
+    h=Math.imul(h+s.charCodeAt(i)|0, 2654435761);
+  return (h^h>>>17)>>>0;
 };
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
 var rmCookie = function(name) {
-    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 };
 
 function newAccount() {
@@ -39,7 +39,8 @@ function newAccount() {
   else if (passwd.value === document.getElementById('passVerify').value) {
     socket.emit('new user', [user.value,
     hashCode(passwd.value),
-    document.getElementById('joincode').value])
+    document.getElementById('joincode').value]);
+    console.log(hashCode(passwd.value));
   }
 }
 
