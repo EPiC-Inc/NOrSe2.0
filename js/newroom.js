ce72c8108531ef403e66ac3fabf139407eb01a1a
[R3dact3d] <emmettwb@gmail.com>
2018-09-21 08:08:46 -0600

    Update newroom.js

--- js/newroom.js
+++ js/newroom.js
@@ -1,1 +1,7 @@
-//test
+var socket = io();
+var roomNameElement = document.getElementById('roomname');
+
+function addRoom() {
+  roomName = roomNameElement.value;
+  socket.emit('new room', roomName);
+}
